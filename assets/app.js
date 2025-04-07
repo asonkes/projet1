a7ab4d7fa2de93e65727e5500414cddf4b5cf54a
asonkes <audrey.sonkes@gmail.com>
2025-04-07 22:03:45 +0200

    JS fonctionne + menu burger  ok

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,18 @@
 import './bootstrap.js';
 import './styles/index.scss';
 
+const headerBurger = document.querySelector('.icon_burger');
+const headerCross = document.querySelector('.icon_cross');
+const burger = document.querySelector('.burger');
+
+headerBurger.addEventListener('click', () => {
+    burger.classList.add('active');
+    headerCross.classList.add('active');
+    headerBurger.classList.add('active');
+});
+
+headerCross.addEventListener('click', () => {
+    burger.classList.remove('active');
+    headerCross.classList.remove('active');
+    headerBurger.classList.remove('active');
+})
