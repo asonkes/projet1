bec3310a0211a2ecfc81bd115f357b371e646bed
asonkes <audrey.sonkes@gmail.com>
2025-04-09 15:59:04 +0200

    liste des tâches s'affichent - doit encore mettre un filtre en fonction de l'id

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProjectRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class TaskController extends AbstractController
 {
     #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    //#[Route('/task/{id}', name: 'app_task')]
+    public function index(ProjectRepository $projectRepository): Response
     {
         // On récupère l'utilisateur
         $user = $this->getUser();
@@ -19,8 +21,30 @@ final class TaskController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
+        // On récupère le projet de l'utilisateur par ID
+        //$project = $projectRepository->find($id);
+
+        // On récupère le projet de l'utilisateur
+        $project = $projectRepository->findBy(['user' => $user]);
+
+        // On récupère les tâches des projets
+        $tasks = [];
+
+        // On boucle pour sur chaque projet de l'utilisateur
+        foreach ($project as $project) {
+
+            // Ici, on boucle sur le projet sélectionné (on fait une sélection sur le projet existant)
+            $projectTasks = $project->getProjectTasks();
+
+            // Et ici, on va récupérer l'id des tâches correspondantes en fonction du projet sélectionné plus haut
+            foreach ($projectTasks as $projectTask) {
+                $tasks[] = $projectTask->getTask();
+            }
+        }
+
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'project' => $project,
+            'tasks' => $tasks
         ]);
     }
 }
