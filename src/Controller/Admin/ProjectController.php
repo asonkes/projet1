092a3ec11b77d952660bd835514f7d77328433f4
asonkes <audrey.sonkes@gmail.com>
2025-04-14 08:46:18 +0200

    securité avec les voters

--- src/Controller/Admin/ProjectController.php
+++ src/Controller/Admin/ProjectController.php
@@ -2,16 +2,43 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Project;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('admin/project', name: 'admin_project_')]
 final class ProjectController extends AbstractController
 {
 
-    #[Route('admin/project', name: 'admin_project')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
+        return $this->render('admin/project/index.html.twig');
+    }
+
+    #[Route('ajout', name: 'add')]
+    public function add(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        return $this->render('admin/project/index.html.twig');
+    }
+
+    #[Route('modifier/{id}', name: 'edit')]
+    public function edit(Project $project): Response
+    {
+        // On vérifie si l'utilisateur peut éditer avec le "voter"
+        $this->denyAccessUnlessGranted('PROJECT_EDIT', $project);
+
+        return $this->render('admin/project/index.html.twig');
+    }
+
+    #[Route('suppression/{id}', name: 'delete')]
+    public function delete(Project $project): Response
+    {
+        // On vérifie si l'utilisateur peut supprimer avec le "voter"
+        $this->denyAccessUnlessGranted('PROJECT_DELETE', $project);
+
         return $this->render('admin/project/index.html.twig');
     }
 }
---
+++ src/Security/Voter/ProjectsVoter.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Project;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class ProjectsVoter extends Voter
+{
+    const EDIT = 'PROJECT_EDIT';
+    const DELETE = 'PROJECT_DELETE';
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    // booléen pk => on se poser la question , est ce que telle ou telle personne a le droit de faire ça??? true ou false
+    // On a met dans un tableau un retour (de edit ou delete) ==> si on a pas ça ==> false (une autre personne que admin ne pourra pas se connecter)
+    protected function supports(string $attribute, $project): bool
+    {
+        if (!in_array($attribute, [self::EDIT, self::DELETE])) {
+            return false;
+        }
+        if (!$project instanceof Project) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // booléen pk => on se poser la question , est ce que telle ou telle personne a le droit de faire ça??? true ou false
+    // 
+    protected function voteOnAttribute($attribute, $project, TokenInterface $token): bool
+    {
+        // On récupère l'utilisateur à partir du token
+        $user = $token->getUser();
+
+        // On vérifie si l'utilisateur est une instance de "userInterface"
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // On vérifie si l'utilisateur ets un "admin"
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        // Si l'utilisateur est connecté mais n'est pas "admin", on vérifie ses permissions
+        switch ($attribute) {
+            case self::EDIT:
+                // On vérifie si l'utilisateur peut éditer 
+                return $this->canEdit();
+                break;
+            case self::DELETE:
+                // On vérifie si l'utilisateur peut supprimer
+                return $this->canDelete();
+                break;
+        }
+
+        return false;
+    }
+
+    private function canEdit()
+    {
+        return $this->security->isGranted('ROLE_PROFILE_MANAGER');
+    }
+
+    private function canDelete()
+    {
+        return $this->security->isGranted('ROLE_PROFILE_MANAGER');
+    }
+}
